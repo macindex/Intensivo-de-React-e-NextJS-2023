3398025a1920d6d4bb02ad432e61579caf0527c2
macindex <machollanda@gmail.com>
2023-05-09 10:02:02 -0300

    setSize image

--- initial-app2/src/components/hooks/aleatoryImage.tsx
+++ initial-app2/src/components/hooks/aleatoryImage.tsx
@@ -1,31 +1,44 @@
 import Image from "next/image"
+import { useState } from "react"
 
 export default function AleatoryImage(){
-    const url = 'https://source.unsplash.com/featured/200x200'
-    let search: string = '' 
+    const [search, setSearch ] = useState<string>('')
+    const [size, setSize ] = useState<number>(300)
+  
+    const url = 'https://source.unsplash.com/featured/'
+    // let search: string = '' 
 
-    function imageUrl(){
-        return `${url}${search}`
-    }
+    // function imageUrl(){
+    //     return `${url}${search}`
+    // }
 
     function renderButton(value: string){
         return(
             <button className={`
             bg-blue-500 px-4 py-2 rounded-md
             `} onClick={() => {
-                search = value 
-                console.log(imageUrl)}}>
+                setSearch(value) 
+                console.log(value)}}>
                 {value}
             </button>
         )
     }
     return (
         <div className="flex flex-col gap-3 border border-zinc-500 p-7 rounded-md">
-            <Image src={imageUrl()} height={200} width={200} alt="Imagem" />
-            <div className="flex justify-between gap-5">
+            <Image src={`${url}${size}x${size}?${search}`} height={300} width={300} alt="Imagem" />
+            <div className="flex gap-5">
             {renderButton('abstract')}
             {renderButton('city')}
             {renderButton('person')}
+            </div>
+            <div>
+                <input 
+                type="number" 
+                value={size} 
+                className='bg-zinc-800 outline-none'
+                onChange={e => {
+                    setSize(+e.target.value)
+                }} />
             </div>
         </div>
     )
