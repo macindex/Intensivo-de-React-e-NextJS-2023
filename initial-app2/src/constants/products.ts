979162c106cf675bbfeb50dc9c9902096fc5afe3
macindex <machollanda@gmail.com>
2023-05-11 09:13:17 -0300

    constants

---
+++ initial-app2/src/constants/products.ts
@@ -0,0 +1,21 @@
+import Product from "@/model/Product";
+
+const products: Product[] = [
+    {
+        id: 1,
+        name: "Camiseta",
+        price: 39.9,
+        description: "Camisa branca de algodão",
+        image: "https://source.unsplash.com/featured/300x300?tshirt"
+    },
+    {
+        id: 2,
+        name: "Calça jeans",
+        price: 99.9,
+        description: "Calça azul de algodão",
+        image: "https://source.unsplash.com/featured/300x300?jeans"
+    }
+
+]
+
+export default products
