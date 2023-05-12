736651f08e415ea3844d6431cd5a557fb4cc7215
macindex <machollanda@gmail.com>
2023-05-12 19:07:10 -0300

    Cart adjusts

--- initial-app2/src/components/lists/Cart.tsx
+++ initial-app2/src/components/lists/Cart.tsx
@@ -1,23 +1,37 @@
 import ItemCart from "@/model/ItemCart";
+import { IconCircleX } from "@tabler/icons-react";
+import { Emblema_One } from "next/font/google";
 import CartItem from "./CartItem";
+import EmptyCart from "./EmptyCart";
 
 interface CartProps{
     itens: ItemCart[]
 }
 
 export default function Cart(props: CartProps){
- return (
-    <div>
-        {props.itens.map((item, i) => {
-            return (
-                <div>
-                     {props.itens.map((item, i) => {
-                        return <CartItem key={i} {...item} />
-                     })}
+ const total = props.itens.reduce((som, item) => {
+  return som + item.amount * item.product.price
+ }, 0)
+  return (
+   <div className="flex flex-col border border-white overflow-hidden w-4/5">
+     <div className="bg-zinc-800 text-3xl p-3">
+      <span>Cart</span>
+      <span>Cart</span>
+      </div>
+     <div className="flex gap-5">
+      {props.itens.length === 0 ? (
+        // <div className="flex gap-3 justify-center text-zinc-500">
+        //   <IconCircleX />
+        //   <span>No item in the cart!</span>
+        // </div>
+        <EmptyCart />
+      ): (
+        props.itens.map((item, i) => {
+          return <CartItem key={i} {...item} />
+        })
+      )}
 
-                </div>
-            )
-        })}
-    </div>
- )
+     </div>
+   </div>
+ );
 }
---
+++ initial-app2/src/components/lists/ProductItem.tsx
@@ -0,0 +1,42 @@
+import products from "@/constants/products";
+import Product from "@/model/Product";
+import { IconShoppingCart } from "@tabler/icons-react";
+import Image from 'next/image'
+
+interface ProductItemProps{
+    product: Product
+    buy: (product: Product) => void
+}
+
+export default function ProductItem(props: ProductItemProps){
+    const { product } = props
+    return (
+      <div
+        className={`
+        flex flex-col rounded-md border border-zinc-600
+        `}
+      >
+        <Image
+          src={props.product.image}
+          width={300}
+          height={200}
+          alt="Imagem do produto"
+          className="rounded-md"
+        />
+        <div className="flex flex-col p-3">
+          <div className="flex justify-between items-center">
+            <div className="text-2xl font-black">{product.name}</div>
+            <div className="text-green-500 font-bold">R${product.price}</div>
+          </div>
+          <div>
+            <div className="text-gray-500">{product.description}</div>
+          </div>
+          <div>
+            <button className="botao flex justify-center gap-2 w-full"
+            onClick={() => props.buy(product)} >
+                <IconShoppingCart />Buy</button>
+          </div>
+        </div>
+      </div>
+    );
+}
